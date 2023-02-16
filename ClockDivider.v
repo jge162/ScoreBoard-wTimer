44c70311d7e3eae166985fdac96c409f19018b5b
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-16 02:58:31 -0800

    Update ClockDivider.v

--- ClockDivider.v
+++ ClockDivider.v
@@ -1,4 +1,5 @@
-`timescale 1ns / 1ps
+timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
 // school: CSUF
 // Engineers: Jeremy, Duy, Spencer
@@ -13,23 +14,29 @@
 //////////////////////////////////////////////////////////////////////////////////
    
 module clock_divider(
-input clk,
-input reset,
-output reg sclk
+  input clk,
+  input reset,
+  output reg sclk
 );
-reg [31:0] count;
-always@(posedge clk or negedge reset)
-begin
-if(reset == 1'b0) begin
-    count <= 32'd0;
-sclk <= 1'b0;
-end else begin
-if(count == 32'd9000000) begin
-    count <= 32'd0;
-sclk <= ~sclk;
-end else begin
-    count <= count + 1;
-end
-end
-end
+
+  reg [31:0] count;
+  
+  always @(posedge clk or negedge reset) begin
+    if (reset == 1'b0) begin
+      count <= 32'd0;
+      sclk <= 1'b0;
+    end else begin
+      if (count == 32'd9000000) begin
+        count <= 32'd0;
+        sclk <= ~sclk;
+      end else begin
+        count <= count + 1;
+      end
+    end
+  end
+  
+  always @(posedge clk) begin
+    $display("sclk = %b", sclk);
+  end
+  
 endmodule

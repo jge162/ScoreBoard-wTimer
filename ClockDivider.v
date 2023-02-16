c1fd5d96c12ef4c9bea8969f9f8fc8a2d21608e0
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-16 03:28:58 -0800

    Update ClockDivider.v

--- ClockDivider.v
+++ ClockDivider.v
@@ -1,42 +1,56 @@
 module clock_divider(
   input clk,
+  input reset,
   output reg sclk
 );
 
   reg [31:0] count;
   
-  always @(posedge clk) begin
-    if (count == 31'd0) begin
-      sclk <= ~sclk;
-      count <= 31'd1;
+  always @(posedge clk or negedge reset) begin
+    if (reset == 1'b0) begin
+      count <= 32'd0;
+      sclk <= 1'b0;
     end else begin
-      count <= count + 1;
+      if (count == 32'd9000000) begin
+        count <= 32'd0;
+        sclk <= ~sclk;
+      end else begin
+        count <= count + 1;
+      end
     end
+  end
+  
+  always @(posedge clk) begin
     $display("sclk = %b", sclk);
   end
   
 endmodule
 
 
-module testbench;
-  reg clk = 0;
-  always #5 clk = ~clk;
+module clock_divider(
+  input clk,
+  input reset,
+  output reg sclk
+);
 
-  reg reset = 1;
-  wire sclk;
-
-  clock_divider dut(
-    .clk(clk),
-    .reset(reset),
-    .sclk(sclk)
-  );
-
-  initial begin
-    #10 reset = 0;
+  reg [31:0] count;
+  
+  always @(posedge clk or negedge reset) begin
+    if (reset == 1'b0) begin
+      count <= 32'd0;
+      sclk <= 1'b0;
+    end else begin
+      if (count == 32'd9000000) begin
+        count <= 32'd0;
+        sclk <= ~sclk;
+      end else begin
+        count <= count + 1;
+      end
+    end
   end
-
+  
   always @(posedge clk) begin
     $display("sclk = %b", sclk);
   end
-
+  
 endmodule

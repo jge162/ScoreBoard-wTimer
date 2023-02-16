20aac4ab6e75acfe2b0acff649844fb91f695556
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-16 03:47:01 -0800

    Update ClockDivider.v

--- ClockDivider.v
+++ ClockDivider.v
@@ -1,4 +1,4 @@
-/*module clock_divider(
+module clock_divider(
   input clk,
   input reset,
   output reg sclk
@@ -43,45 +43,4 @@ module tb_clock_divider;
   end
   
   always #5 clk = ~clk;
-endmodule*/
-module half_adder
-(
-    input wire a, b,
-    output wire sum, carry
-);
-
-assign sum = a ^ b;
-assign carry = a & b;
-
-endmodule
-
-module half_adder_tb;
-
-    reg a, b;
-    wire sum, carry;
-
-    // duration for each bit = 20 * timescale = 20 * 1 ns  = 20ns
-    localparam period = 20;  
-
-    half_adder UUT (.a(a), .b(b), .sum(sum), .carry(carry));
-    
-    initial // initial block executes only once
-        begin
-            // values for a and b
-            a = 0;
-            b = 0;
-            #period; // wait for period 
-
-            a = 0;
-            b = 1;
-            #period;
-
-            a = 1;
-            b = 0;
-            #period;
-
-            a = 1;
-            b = 1;
-            #period;
-        end
 endmodule
